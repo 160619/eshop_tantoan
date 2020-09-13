71be5652440e5bbbe0f3b99a5245e97f16e6aab6
tantoan <nguyentantoan999@gmail.com>
2020-09-13 09:16:18 +0700

    fix_comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,15 @@
 class CommentsController < ApplicationController
   def new
 
-    @review = Review.find(params[:product_id])
+    @review = Review.find(params[:review_id])
     @comment = @review.comments.new
   end
 
   def create
-    @review = Review.find(params[:product_id])
+    @review = Review.find(params[:review_id])
     @comment = @review.comments.new(comment_params)
     if @comment.save
-      redirect_to product_path(@comment.review), notice: 'created comment successfully'
+      redirect_to product_path(@review.product_id), notice: 'created comment successfully'
     else
       render 'products/show'
     end
@@ -18,6 +18,6 @@ class CommentsController < ApplicationController
 private
 
   def comment_params
-    params.require(:comment).permit :body
+    params.require(:comment).permit :body, :review_id
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews
   end
-  resources :products do
+  resources :reviews, only: %[] do
     resources :comments
   end
 
