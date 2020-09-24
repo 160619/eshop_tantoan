4e0512955078599ed830006290841371f8f78633
tantoan <nguyentantoan999@gmail.com>
2020-09-24 20:58:22 +0700

    refactor_review_ajax

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,15 +8,16 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        @product.calculate_rating
-        format.html {redirect_to @review.product, notice: 'Review was successfully created' }
-        format.js
-        format.json {render json: @review, status: :created, location: @user}
-      else
-        format.html { render 'products/show' }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
+      html_comment_form = render_to_string(
+        partial: 'comments/form',
+        locals: {review: @review, comment: Comment.new}
+      )
+      @product.calculate_rating
+      format.json { render json: review_serializer(html_comment_form), status: :created }
+     else
+       format.json { render json: @review.errors.full_messages, status: :unprocessable_entity }
+     end
+   end
   end
 
   def show
@@ -30,4 +31,26 @@ private
   def review_params
     params.require(:review).permit :content, :rating
   end
+
+  def review_serializer(comment_form)
+      {
+        id: @review.id,
+        content: @review.content,
+        rating: @review.rating,
+        user: {
+          email: @review.user.email
+        },
+        product: {
+          aggregate_rating: @product.reload.aggregate_rating
+        },
+        comment_form: comment_form
+      }
+    end
+
+    def comment_form_html
+      render_to_string(
+        partial: 'comments/form',
+        locals: { review: @review, comment: Comment.new }
+      )
+    end
 end
