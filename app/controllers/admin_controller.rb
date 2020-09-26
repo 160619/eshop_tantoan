6beeb309a146f9abab5fc60eb6d6d8f15012afd1
tantoan <nguyentantoan999@gmail.com>
2020-09-26 21:18:02 +0700

    admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,14 @@
 class AdminController < ActionController::Base
   before_action :authenticate_user!
+  before_action :admin_verify!
   layout 'application'
 
   def index
+  end
+
+  private
+    def admin_verify!
+      return true if current_user.role == "admin"
+      redirect_to root_path, alert: 'you do not admin '
   end
 end
--- lib/tasks/db.rake
+++ lib/tasks/db.rake
@@ -1,7 +1,8 @@
 namespace :db do
   desc "TODO"
   task fake_data: :environment do
-    
+    User.create(email:'demoadmin@gmail.com', password:'111111', password_confirmation: '111111', role:'admin')
+
     arr_category_names = %w[samsung macbook sony lg iphone asus ipa hp lenovo ]
 
     arr_category_names.each do |category_name|
